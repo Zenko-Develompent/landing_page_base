a097fa2269c9f7ca1d9453912f2471794eef3120
karri <nekomarait@gmail.com>
2025-10-27 21:28:44 +0500

    Вот нормальный проект (даже есть название странички и лого)

---
+++ svelte_project/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/zenko_icon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children?.()}
---
+++ svelte_project/src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<title>Zenko</title>
+ 
